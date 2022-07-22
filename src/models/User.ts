42a45911154d9497dae2b13c6ee3721b3abfd7f7
Udoka <udochukwukaonyela@gmail.com>
2022-07-22 19:49:25 +0100

    created controller with typescript

---
+++ src/models/User.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+
+interface UserSchema{
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    phoneNumber: string;
+    userName: string;
+}
+
+const UserSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: {
+        type: String, required: true
+    },
+    phoneNumber: String,
+    userName: String
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
