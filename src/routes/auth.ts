a0448d4a33fd984bba0aa853ae914ddadae93bd4
Udoka <udochukwukaonyela@gmail.com>
2022-07-22 19:51:21 +0100

    created route with typescript

---
+++ src/routes/auth.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction, Router } from "express";
+const router = Router();
+
+
+router.get('/signin', (req:Request, res:Response, next:NextFunction) => {
+    return res.status(200).send('Everything fine here!')
+})
+router.get('/login', (req:Request, res:Response, next:NextFunction) => {
+    return res.status(200).send('Everything fine here!')
+})
+
+
+module.exports = router;
