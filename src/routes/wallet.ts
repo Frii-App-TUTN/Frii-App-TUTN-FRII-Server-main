9514e899594360a23bf610a61fdace2231f07fb7
Udoka <udochukwukaonyela@gmail.com>
2022-07-25 17:39:10 +0100

    wallet route

--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction, Router } from "express";
 const router = Router();
 
 
-router.get('/signin', (req:Request, res:Response, next:NextFunction) => {
+router.get('/create', (req:Request, res:Response, next:NextFunction) => {
     return res.status(200).send('Everything fine here!')
 })
 router.get('/login', (req:Request, res:Response, next:NextFunction) => {
