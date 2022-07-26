2b1808144dba70117b20036e4304b176f63571b8
Udoka <udochukwukaonyela@gmail.com>
2022-07-26 10:29:24 +0100

    created wallet routes

--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -1,9 +1,41 @@
 import { Request, Response, NextFunction, Router } from "express";
 const router = Router();
+const Wallet = require('../models/Wallet');
 
+interface Wallet {
+    error?: string;
+    success?: string;
+} 
+router.get('/create', async (req: Request,
+    res: Response<Wallet>, next: NextFunction) => {
+    const { customerID, accountName, currency, accountOpeningDate, lastTransactionDate, userName, purses, emailAddress } = req.body;
+    try {
+          let wallet = await Wallet.findOne({ emailAddress });
+          if (wallet) {
+            res.status(500).json({error: "Wallet Already exists"});
+          } else {
+              wallet = new Wallet(
+                  {
+                      customerID,
+                      accountName,
+                      currency,
+                      accountOpeningDate,
+                      lastTransactionDate,
+                      userName,
+                      purses,
+                      emailAddress
+                  }
+            
+            );
+            await wallet.save();
 
-router.get('/create', (req:Request, res:Response, next:NextFunction) => {
-    return res.status(200).send('Everything fine here!')
+            res.status(200).json({success: "Created wallet succesfully"});
+          }
+        } catch (err) {
+          console.error(err);
+          res.status(500).json({error: 'Server error'});
+        }
+    
 })
 router.get('/login', (req:Request, res:Response, next:NextFunction) => {
     return res.status(200).send('Everything fine here!')
