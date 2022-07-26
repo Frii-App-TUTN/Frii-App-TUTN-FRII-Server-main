4de141a22801496aeb904def2f957c01cf21df90
Udoka <udochukwukaonyela@gmail.com>
2022-07-26 10:40:53 +0100

    created hjelpers

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -2,11 +2,12 @@ require('dotenv').config();
 import {createHmac} from 'crypto';
 
 interface Helpers  {
-    hash?: (str:string|undefined) => string|boolean;
+    hash?: (str?:string) => string|boolean;
+    createRandomString?: (strLength?:number|boolean) => string|boolean;
 }
 const secretKey: string|undefined = process.env.SECRET_HASH;
 const helpers: Helpers = {}
-helpers.hash = (str:string|undefined):string|boolean => {
+helpers.hash = (str) => {
     if (typeof (str) == 'string' && str.length > 0) {
         var hash = createHmac('sha256', String(secretKey)).update(str).digest('hex');
         return hash;
@@ -14,6 +15,31 @@ helpers.hash = (str:string|undefined):string|boolean => {
         return false;
     }
 };
+helpers.createRandomString = (strLength) => {
+  strLength = typeof (strLength) == 'number' && strLength > 0 ? strLength : false;
+    if (strLength) {
+      
+    // Define all the possible characters that can go into a string;
+      
+    var possibleCharacters = 'abcdefghijklmnopqrstuvwxyxz';
+    var lastTwoNumbers = '1234567890';
 
+    var str = '';
+
+      for (let i = 1; i <= strLength; i++) {
+        
+      // Get random character from the possible string
+        
+      var randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+      // Append this character to the final string
+      str += randomCharacter;
+    }
+    //return the final string
+    return str;
+
+  } else {
+    return false;
+  }
+};
 module.exports = helpers;
 
