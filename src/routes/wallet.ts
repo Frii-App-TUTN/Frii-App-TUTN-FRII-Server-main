a9c6b036d9ada06c17b198672dddc2ae4947db01
Udoka <udochukwukaonyela@gmail.com>
2022-07-27 20:03:54 +0100

    fixes

--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -4,12 +4,13 @@ import axios from "axios";
 const router = Router();
 const Wallet = require('../models/Wallet');
 const randomChar = require('../helpers/helpers').createRandomString;
+console.log(require('../helpers/wallet/create').createUser);
 interface Wallet {
     error?: boolean;
     message?: string;
 }
-router.post('/create', 
-    require('../helpers/wallet/create')
+router.post('/create',
+  require('../helpers/wallet/create').createUser
 )
 router.get('/login', (req:Request, res:Response, next:NextFunction) => {
     return res.status(200).send('Everything fine here!')
