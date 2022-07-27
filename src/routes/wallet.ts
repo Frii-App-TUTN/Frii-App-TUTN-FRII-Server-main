cd3bdf4f0e2d3fe7a8e16903bfc86674c726f783
Udoka <udochukwukaonyela@gmail.com>
2022-07-27 13:23:44 +0100

    fixes

--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -8,7 +8,7 @@ interface Wallet {
 } 
 router.get('/create', async (req: Request,
     res: Response<Wallet>, next: NextFunction) => {
-    // send request to paystack
+    // send request to 
     const {accountName, currency, accountOpeningDate, lastTransactionDate, userName, emailAddress } = req.body;
     try {
           let wallet = await Wallet.findOne({ emailAddress });
