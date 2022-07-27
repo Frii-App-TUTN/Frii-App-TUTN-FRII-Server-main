5b9da37eaa25a301bff2565767ee38b00ee3bac6
Udoka <udochukwukaonyela@gmail.com>
2022-07-27 20:28:28 +0100

    fixed up logic

--- src/helpers/wallet/create.ts
+++ src/helpers/wallet/create.ts
@@ -1,49 +1,69 @@
 require('dotenv').config()
 import { Request, Response, NextFunction, Router } from "express";
 import { MongooseError } from "mongoose";
-import { UserSchema } from "../../models/User";
+import { User, UserSchema } from "../../models/User";
+interface Wallet {
+    error?: boolean;
+    message?: string;
+}
 exports.createUser = async (req: Request,
-    res: Response, next: NextFunction) => {
+    res: Response<Wallet>
+    ,next: NextFunction) => {
     // send request to 
     console.log(process.env.PAYSTACK_SECRET_KEY);
     const { accountName, currency, accountOpeningDate, lastTransactionDate, userName, emailAddress } = req.body;
-
     try {
-        const https = require('https')
+        const user = await User.findOne<UserSchema>({ email: emailAddress?.toLowerCase() });
+        if (!!user) {
+            const { firstName, lastName, phoneNumber } = user;
+            const https = require('https');
 
-        const params = JSON.stringify({
-            "customer": 481193,
-            "preferred_bank": "access-bank",
-        })
-
-        const options = {
-            hostname: 'api.paystack.co',
-            port: 443,
-            path: '/dedicated_account',
-            method: 'POST',
-            headers: {
-                Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-                'Content-Type': 'application/json'
-            }
-        }
-
-        let data = ''
-        const req = https.request(options, (res: any) => {
-
-            res.on('data', (chunk: any) => {
-                data += chunk
-            });
-
-            res.on('end', () => {
-                console.log(JSON.parse(data))
+            const params = JSON.stringify({
+                "customer": 481193,
+                "preferred_bank": "access-bank",
+                "first_name": firstName,
+                "last_name": lastName,
+                "phone": phoneNumber
             })
-        }).on('error', (error: any) => {
-            console.error(error)
-        })
 
-        req.write(params)
-        req.end()
-        res.status(200).json('success');
+            const options = {
+                hostname: 'api.paystack.co',
+                port: 443,
+                path: '/dedicated_account',
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+                    'Content-Type': 'application/json'
+                }
+            }
+
+            let data = ''
+            const req = await https.request(options, (res: any) => {
+
+                res.on('data', (chunk: any) => {
+                    data += chunk
+                });
+
+                res.on('end', () => {
+                    data = JSON.parse(data);
+                })
+            }).on('error', (error: any) => {
+                console.error(error)
+            })
+
+            req.write(params)
+            req.end()
+            res.status(201).json({
+                error: false,
+                message: 'success'
+            });
+        }
+        else {
+            res.status(404).json({
+                error: true,
+                message: "User with email address does not exist"
+            })
+        }
         //         
         //       );
         //         console.log(response);
