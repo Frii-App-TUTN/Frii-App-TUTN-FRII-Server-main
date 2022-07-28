e807bb8a36bce09dbddbbb81181133dfa9e3aa4a
Udoka <udochukwukaonyela@gmail.com>
2022-07-28 21:38:52 +0100

    fixes

--- src/helpers/wallet/create.ts
+++ src/helpers/wallet/create.ts
@@ -5,6 +5,7 @@ import { User, UserSchema } from "../../models/User";
 import { Customer, Wallet, WalletSchema, customerSchema } from '../../models/Wallet';
 const https = require('https');
 const createCustomer = require('./customers').createCustomer;
+const createAccount = require('./customers').createAccount;
 interface Wallet {
     error?: boolean;
     message?: string;
@@ -19,56 +20,62 @@ exports.createWallet = async (req: Request,
             const user = await User.findOne<UserSchema>({ email: emailAddress?.toLowerCase() });
             if (!!user) {
                 const { firstName: first, lastName:last, phoneNumber:phone } = user;
-                createCustomer(first, last, phone, async (data:any) => {
-                    if (data) {
-                        const { id: Id } = data;
+                createCustomer(first, last, phone, async (err: boolean, data: any) => {
+                    if (!err) {
+                        if (data) {
+                            const { id: Id } = data;
                     
  
-                        res.status(201).json({
-                            error: false,
-                            message: 'success'
-                        });
-                        const {
-                            id,
-                            account_name: accountName,
-                            account_number: accountNumber,
-                            currency,
-                            created_at: createdAt,
-                            updated_at: updatedAt,
-                            customer
-                        } = data?.data;
-                        type customerRes = {
-                            id: number;
-                            first_name: string;
-                            last_name: string;
-                            customer_code: string;
-                            phone: string;
-                            risk_action: string;
+                            res.status(201).json({
+                                error: false,
+                                message: 'success'
+                            });
+                            const {
+                                id,
+                                account_name: accountName,
+                                account_number: accountNumber,
+                                currency,
+                                created_at: createdAt,
+                                updated_at: updatedAt,
+                                customer
+                            } = data?.data;
+                            type customerRes = {
+                                id: number;
+                                first_name: string;
+                                last_name: string;
+                                customer_code: string;
+                                phone: string;
+                                risk_action: string;
+                            }
+                            const {
+                                id: customerId,
+                                first_name: firstName,
+                                last_name: lastName,
+                                customer_code: customerCode,
+                                phone: phoneNumber,
+                                risk_action: riskAction
+                            }: customerRes = customer;
+                            const customerSchema = await new Customer<customerSchema>({ id: customerId, firstName, lastName, customerCode, phoneNumber, riskAction })
+                            const wallet = await new Wallet<WalletSchema>({
+                                id,
+                                emailAddress,
+                                accountName,
+                                accountNumber,
+                                currency,
+                                createdAt,
+                                updatedAt,
+                                customer: customerSchema
+                            });
+                            wallet.save((err) => {
+                                if (err) return res.status(507).json({ error: true, message: "Error saving wallet" })
+                            });
                         }
-                        const {
-                            id: customerId,
-                            first_name: firstName,
-                            last_name: lastName,
-                            customer_code: customerCode,
-                            phone: phoneNumber,
-                            risk_action: riskAction
-                        }: customerRes = customer;
-                        const customerSchema = await new Customer<customerSchema>({ id: customerId, firstName, lastName, customerCode, phoneNumber, riskAction })
-                        const wallet = await new Wallet<WalletSchema>({
-                            id,
-                            emailAddress,
-                            accountName,
-                            accountNumber,
-                            currency,
-                            createdAt,
-                            updatedAt,
-                            customer: customerSchema
-                        });
-                        wallet.save((err) => {
-                            if (err) return res.status(507).json({ error: true, message: "Error saving wallet" })
-                        });
+                    } else {
+                        res.status(404).json({
+                            error: true,
+                            message: "User with email address does not exist"
+                        })        
                     }
-                        
                 });
                     
             }
