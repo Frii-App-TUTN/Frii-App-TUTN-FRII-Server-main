82e9273d7056d2ec3d8179fa74caac6c6f49675e
Udoka <udochukwukaonyela@gmail.com>
2022-07-28 18:55:10 +0100

    fixes

--- src/helpers/wallet/create.ts
+++ src/helpers/wallet/create.ts
@@ -14,94 +14,63 @@ exports.createWallet = async (req: Request,
     ,next: NextFunction) => {
     // send request to 
     const { emailAddress } = req.body;
-
     if (emailAddress) {
         try {
             const user = await User.findOne<UserSchema>({ email: emailAddress?.toLowerCase() });
             if (!!user) {
-                const { firstName: first, lastName: last, phoneNumber: phone } = user;
-
-                const params = JSON.stringify({
-                    "preferred_bank": "access-bank",
-                    "first_name": first,
-                    "last_name": last,
-                    "phone": phone
-                })
-
-                const options = {
-                    hostname: 'api.paystack.co',
-                    port: 443,
-                    path: '/dedicated_account',
-                    method: 'POST',
-                    headers: {
-                        Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-                        'Content-Type': 'application/json'
+                const { firstName: first, lastName:last, phoneNumber:phone } = user;
+                createCustomer(first, last, phone, async (data:any) => {
+                    if (data) {
+                        const { id: Id } = data;
+                    
+ 
+                        res.status(201).json({
+                            error: false,
+                            message: 'success'
+                        });
+                        const {
+                            id,
+                            account_name: accountName,
+                            account_number: accountNumber,
+                            currency,
+                            created_at: createdAt,
+                            updated_at: updatedAt,
+                            customer
+                        } = data?.data;
+                        type customerRes = {
+                            id: number;
+                            first_name: string;
+                            last_name: string;
+                            customer_code: string;
+                            phone: string;
+                            risk_action: string;
+                        }
+                        const {
+                            id: customerId,
+                            first_name: firstName,
+                            last_name: lastName,
+                            customer_code: customerCode,
+                            phone: phoneNumber,
+                            risk_action: riskAction
+                        }: customerRes = customer;
+                        const customerSchema = await new Customer<customerSchema>({ id: customerId, firstName, lastName, customerCode, phoneNumber, riskAction })
+                        const wallet = await new Wallet<WalletSchema>({
+                            id,
+                            emailAddress,
+                            accountName,
+                            accountNumber,
+                            currency,
+                            createdAt,
+                            updatedAt,
+                            customer: customerSchema
+                        });
+                        wallet.save((err) => {
+                            if (err) return res.status(507).json({ error: true, message: "Error saving wallet" })
+                        });
                     }
-                }
-
-                let data: any = '';
-                const req = await https.request(options, (res: any) => {
-
-                    res.on('data', (chunk: any) => {
-                        data += chunk
-                    });
-
-                    res.on('end', () => {
-                        data = JSON.parse(data);
-                    })
-                }).on('error', (error: any) => {
-                    res.status(502).json({
-                        error: true,
-                        message: error.message,
-                    });
-                   
-                })
-
-                req.write(params)
-                req.end()
-                res.status(201).json({
-                    error: false,
-                    message: 'success'
+                        
                 });
-                const {
-                    id,
-                    account_name: accountName,
-                    account_number: accountNumber,
-                    currency,
-                    created_at: createdAt,
-                    updated_at: updatedAt,
-                    customer
-                } = data?.data;
-                type customerRes = {
-                    id: number;
-                    first_name: string;
-                    last_name: string;
-                    customer_code: string;
-                    phone: string;
-                    risk_action: string;
-                }
-                const {
-                    id: customerId,
-                    first_name: firstName,
-                    last_name: lastName,
-                    customer_code: customerCode,
-                    phone: phoneNumber,
-                    risk_action: riskAction
-                }: customerRes = customer;
-                const customerSchema = await new Customer<customerSchema>({ id: customerId, firstName, lastName, customerCode, phoneNumber, riskAction })
-                const wallet = await new Wallet<WalletSchema>({
-                    id,
-                    emailAddress,
-                    accountName,
-                    accountNumber,
-                    currency,
-                    createdAt,
-                    updatedAt,
-                    customer: customerSchema
-                });
-                wallet.save((err) => {
-                    if (err) return res.status(507).json({ error: true, message: "Error saving wallet" })
-                });
+                    
             }
             else {
                 res.status(404).json({
