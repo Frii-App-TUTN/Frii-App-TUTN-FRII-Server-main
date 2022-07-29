2af45e041b193b55905d06a53cac2629fa2d2b6f
Udoka <udochukwukaonyela@gmail.com>
2022-07-29 10:01:10 +0100

    fixes to wallet routes

--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -3,9 +3,9 @@ import { Request, Response, NextFunction, Router } from "express";
 const router = Router();
 const Wallet = require('../models/Wallet');
 router.post('/create',
-  require('../helpers/wallet/create').createWallet
+  require('../helpers/wallet/wallet').createWallet
 )
-router.get('/fetch',  )
+router.get('/fetch', require('../helpers/wallet/wallet').fetchWallet)
 
 
 module.exports = router;
