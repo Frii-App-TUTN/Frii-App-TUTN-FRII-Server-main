1c8b90ea8af75fa6965e6d76af9c8010abd17417
Udoka <udochukwukaonyela@gmail.com>
2022-07-29 12:50:00 +0100

    changes

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -23,9 +23,10 @@ app.use(bodyParser.urlencoded(Options));
 app.use(cors());
 
 // Define routes
-app.use('/', require('../routes/index'))
-app.use('/auth', require('../routes/auth'))
-app.use('/wallet', require('../routes/wallet'))
+app.use('/', require('../routes/index'));
+app.use('/auth', require('../routes/auth'));
+app.use("/account", require("../routes/account"));
+app.use('/wallet', require('../routes/wallet'));
 
 
 
