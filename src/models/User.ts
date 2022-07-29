8774707d1f0978fa8c2d970bb444b5f7af10ec45
Udoka <udochukwukaonyela@gmail.com>
2022-07-29 12:47:50 +0100

    fixes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,23 +1,44 @@
 import { Schema, model } from 'mongoose';
 
-export interface UserSchema{
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    phoneNumber: string;
-    userName: string;
+export interface UserSchema {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  userName: string;
+  otp: string;
+  otpCreated: number;
+  emailVerified: boolean;
+  resetPasswordCreated: number;
+  resetPasswordCode: string;
 }
 
-const UserSchema = new Schema<UserSchema>({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: {
-        type: String, required: true
-    },
-    phoneNumber: String,
-    userName: String
+const UserSchema = new Schema({
+  _id: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  otp: String,
+  otpCreated: Number,
+  emailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  resetPasswordCreated: String,
+  resetPasswordCode: String,
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: String,
+  userName: String,
+  created: {
+    type: String,
+    required: false,
+    default: Date.now(),
+  },
 });
 
-export const User = model('User', UserSchema);
+export const User = model("User", UserSchema);
