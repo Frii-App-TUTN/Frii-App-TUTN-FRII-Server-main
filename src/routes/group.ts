7d64ca7c51783958645431d7beb32a05a15fea76
Udoka <udochukwukaonyela@gmail.com>
2022-08-03 16:57:08 +0100

    added join route

--- src/routes/group.ts
+++ src/routes/group.ts
@@ -1,9 +1,11 @@
 require('dotenv').config();
 import { Router } from "express";
 const router = Router();
-const { createGroup, addMember } = require('../controllers/Group');
+const { createGroup, addMember, joinGroup } = require('../controllers/Group');
 router.post('/create', createGroup);
-router.put('/addmember', addMember)
+router.put('/addmember', addMember);
+router.get('/join/:code', joinGroup);
+
 // router.put('/removemember', removeMember)
 // router.get('/fetch', fetchGroup);
 
