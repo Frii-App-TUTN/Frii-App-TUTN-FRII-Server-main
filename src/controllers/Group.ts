b3a11e6d2dbd061bc1c304941729583652e5e0cd
Udoka <udochukwukaonyela@gmail.com>
2022-08-03 09:35:50 +0100

    created group

--- src/controllers/Group.ts
+++ src/controllers/Group.ts
@@ -1,5 +1,16 @@
 require('dotenv').config();
 import { Response, Request } from "express";
-exports.createGroup = (req: Request, res: Response) => {
+import {User, UserSchema} from '../models/User'
+exports.createGroup = async (req: Request, res: Response) => {
     const { emailAddress } = req.body;
+    const user = await User.findOne<UserSchema>({ emailAddress });
+    console.log(user);
+    if (!!user) {
+        
+    } else {
+        return res.status(404).json({
+            error: true,
+            message: "user with email not found"
+        });
+    }
 } 
