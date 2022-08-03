8a1532dbe457613e2b427fb6df5dcb8e6cf6ea12
Udoka <udochukwukaonyela@gmail.com>
2022-08-03 09:13:26 +0100

    creted group schema

--- src/models/Group.ts
+++ src/models/Group.ts
@@ -3,11 +3,19 @@ export interface GroupSchema {
     id: number;
     name: string;
     members: string[];
-    
-
-
+    createdAt: String;
+    disabled: boolean;
+    Admin: string;
 }
 const GroupSchema = new Schema<GroupSchema>({
-    
+    id: Number,
+    name: String,
+    members: [String],
+    createdAt: {
+        type: String,
+        default: Date.now(),
+        required: false
+    },
+    Admin: String,
 });
 export const Group = model('Group', GroupSchema);
