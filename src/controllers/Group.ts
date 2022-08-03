3ccae4e86c7739a338d3ad00821bedc517b41d89
Udoka <udochukwukaonyela@gmail.com>
2022-08-03 11:30:43 +0100

    testing

--- src/controllers/Group.ts
+++ src/controllers/Group.ts
@@ -1,19 +1,39 @@
 require('dotenv').config();
 import { Response, Request } from "express";
-import {User, UserSchema} from '../models/User'
-exports.createGroup = async (req: Request, res: Response) => {
-    const { emailAddress } = req.body;
-    const user = await User.findOne<UserSchema>({ emailAddress });
-    console.log(user);
-    if (!!user) {
-        res.status(200).json({
-            error: false,
-            message: "user found"
-        })
+import { User, UserSchema } from '../models/User'
+import { GroupSchema, Group } from '../models/Group';
+const {createRandomNumber} = require('../helpers/helpers');
+interface Group {
+    error: boolean;
+    message?: string;
+    data?: any;
+}
+exports.createGroup = async (req: Request, res: Response<Group>) => {
+    const { emailAddress, groupName, } = req.body;
+    if (emailAddress) {
+        const user = await User.findOne<UserSchema>({ emailAddress });
+        console.log(user, User.findOne<UserSchema>({ emailAddress }))
+        if (!!user) {
+            const id = createRandomNumber(16);
+            const idCheck = await Group.findOne({ id });
+            if (!idCheck) {
+                
+            } else {
+                res.status(500).json({
+                    error: true,
+                    message: "id Generated already exists"
+                })
+            }
+        } else {
+            return res.status(404).json({
+                error: true,
+                message: "user with email not found"
+            });
+        }
     } else {
-        return res.status(404).json({
+        res.status(400).json({
             error: true,
-            message: "user with email not found"
-        });
+            message: "invalid request",
+        })
     }
 } 
