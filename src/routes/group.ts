b600505ef68734e1406ad41cd8d23c78d86d9868
Udoka <udochukwukaonyela@gmail.com>
2022-08-03 08:56:07 +0100

    logic for group routes

---
+++ src/routes/group.ts
@@ -0,0 +1,8 @@
+require('dotenv').config();
+import { Router } from "express";
+const router = Router();
+// const { createGroup, fetchGroup } = require('../controllers/');
+// router.post('/create', createGroup);
+// router.put('/addmember', addMember)
+// router.put('/removemember', removeMember)
+// router.get('/fetch', fetchGroup);
