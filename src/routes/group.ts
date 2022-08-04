ec052d7bffed853d82964ad7c2173acdd20492bc
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 22:56:14 +0100

    created group route

--- src/routes/group.ts
+++ src/routes/group.ts
@@ -1,12 +1,13 @@
 require('dotenv').config();
 import { Router } from "express";
 const router = Router();
-const { createGroup, addMember, joinGroup, removeMember } = require('../controllers/Group');
+const { createGroup, addMember, joinGroup, removeMember, fetchGroup, renameGroup } = require('../controllers/Group');
 router.post('/create', createGroup);
 router.put('/addmember', addMember);
 router.get('/join/:code', joinGroup);
 
 router.put('/removemember', removeMember)
-// router.get('/fetch', fetchGroup);
+router.get('/fetch', fetchGroup);
+router.put('/rename', renameGroup);
 
 module.exports = router;
