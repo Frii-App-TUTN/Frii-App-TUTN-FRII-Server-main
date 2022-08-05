bf86b52088b726dbc137cbb10eb96fc2424e115a
Udoka <udochukwukaonyela@gmail.com>
2022-08-05 07:00:00 +0100

    impoered body validator

--- src/routes/group.ts
+++ src/routes/group.ts
@@ -1,8 +1,10 @@
 require('dotenv').config();
 import { Router } from "express";
 const router = Router();
+import { body, validationResult } from 'express-validator';
 const { createGroup, addMember, joinGroup, removeMember, fetchGroup, renameGroup } = require('../controllers/Group');
-router.post('/create', createGroup);
+router.post('/create',
+    createGroup);
 router.put('/addmember', addMember);
 router.get('/join/:code', joinGroup);
 router.put('/removemember', removeMember);
